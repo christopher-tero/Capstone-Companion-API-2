2f86ad068d99d77c1207bedb8e6403ac73630b07
Christopher Tero <christopher.tero@gmail.com>
2019-09-16 10:58:18 -0500

    added async/await to methods

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CapstoneCompanion.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CapstoneCompanion.API.Controllers
 {
@@ -19,18 +20,18 @@ namespace CapstoneCompanion.API.Controllers
         }
         // GET api/values
         [HttpGet]
-        public IActionResult GetValues()
+        public async Task<IActionResult> GetValues()
         {
-            var values = _context.Projects.ToList();
+            var values = await _context.Projects.ToListAsync();
 
             return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Projects.FirstOrDefault(x => x.Id == id);
+            var value = await _context.Projects.FirstOrDefaultAsync(x => x.Id == id);
 
             return Ok(value);
         }
